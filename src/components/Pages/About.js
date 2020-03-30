9b5b13701ccea6a99733175edeb128dfd2efcf42
John Doe <huda.prs2002@gmail.com>
2020-03-30 22:42:45 +0700

    router

---
+++ src/components/Pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function About() {
+  return (
+    <React.Fragment>
+      <h1>
+        This is About Page
+      </h1>
+    </React.Fragment>
+  );
+}
+
+export default About;
--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
     <header style={headerStyle}>
       <h1>Todo List</h1>
+      <Link 
+        to='/' 
+        style={linkStyle}
+      >
+        Todos
+      </Link>
+      {' '}
+        |
+      {' '}
+      <Link 
+        to='about'
+        style={linkStyle}
+      >
+        About
+      </Link>
     </header>
   );
 }
@@ -13,6 +29,11 @@ const headerStyle = {
   padding: '10px',
   color: '#fff',
   textAlign: 'center'
+};
+
+const linkStyle = {
+  color: '#fff',
+  textDecoration: 'none'
 };
 
 export default Header;
